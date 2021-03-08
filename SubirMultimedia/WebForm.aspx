42fc003921b2bfa65990351adcc924025592868a
abraham <abrahamaltuve@unisinu.edu.co>
2021-03-07 22:18:54 -0500

    Agregar archivos de proyecto.

---
+++ SubirMultimedia/WebForm.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="SubirMultimedia.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+
+        <asp:FileUpload ID="selectFile" runat="server" />
+        <br />
+            <asp:Button ID="btnPlay" runat="server" OnClick="btnPlay_Click" Text="Reproducir" />
+        
+        
+
+        <p>
+            <asp:Literal ID="videoLiteral" runat="server"></asp:Literal>
+        </p>
+        
+        
+
+    </form>
+</body>
+</html>
---
+++ SubirMultimedia/WebForm.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SubirMultimedia
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnPlay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (selectFile.HasFiles)
+                {
+                    string fileName = selectFile.FileName;
+                    string ext = System.IO.Path.GetExtension(fileName).ToLower();
+                    
+                    
+                    if (ext == ".mp4")
+                    {
+                        
+                        selectFile.SaveAs(Server.MapPath("/media/" + fileName));
+                        string link = "/media/" + fileName;
+                        link = "<video width=600 Controls><Source src=" + link + " type=video/mp4></video>";
+                        videoLiteral.Text = link;
+                        Response.Write("Se subio el archivo MP4 " + fileName);
+
+                    }
+
+                }
+                else
+                {
+                    Response.Write("Seleccione un archivo");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
